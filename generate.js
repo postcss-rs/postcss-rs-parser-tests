9aadb7bf4c17ae8d48718981aa726373aa56a978
IWANABETHATGUY <iwanabethatguy@qq.com>
2021-11-20 17:57:10 +0800

    feat: 🎸 init

---
+++ generate.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+const postcss = require('postcss')
+const file = fs.readFileSync('./official-cases', '')
+const root = po
+
---
+++ jsonify.js
@@ -0,0 +1,19 @@
+function clean(node) {
+  if (node.source) {
+    delete node.source.input
+    delete node.source.inputId
+  }
+  delete node.inputs
+  delete node.indexes
+  delete node.lastEach
+  delete node.rawCache
+
+  if (node.nodes) node.nodes = node.nodes.map(clean)
+
+  return node
+}
+
+module.exports = function jsonify(node) {
+  let cleaned = clean(node.toJSON())
+  return JSON.stringify(cleaned, null, 2)
+}
