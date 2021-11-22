e2a6a3c75440ef2366f8cee80dc3e592d6825b44
IWANABETHATGUY <iwanabethatguy@qq.com>
2021-11-22 23:01:02 +0800

    feat: 🎸 add basic ast gen

--- generate.js
+++ generate.js
@@ -2,10 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const jsonify = require("./jsonify");
 const postcss = require("postcss");
-const file = fs.readFileSync("./custom-cases/simple.css", { encoding: "utf-8" });
-const root = postcss.parse(file);
-
-console.log(generate(root, 0, file ));
+// const file = fs.readFileSync("./custom-cases/simple.css", { encoding: "utf-8" });
 
 function generate(node, level, source) {
   const { type, nodes } = node;
@@ -16,7 +13,7 @@ function generate(node, level, source) {
       break;
     }
     case "decl": {
-      string += generateDeclaration(node, level, source)
+      string += generateDeclaration(node, level, source);
       break;
     }
     case "rule": {
@@ -55,13 +52,13 @@ function generateRoot(node, level, source) {
   string += `${" ".repeat(level * 2)}Root@${start.offset}..${source.length}\n`;
   return string;
 }
-
+// original postcss parser end offset is inclusive, so we need to add one to compatible with rust `range`
 function generateRule(node, level, source) {
   const {
     source: { start, end },
   } = node;
   let string = "";
-  string += `${" ".repeat(level * 2)}Rule@${start.offset}..${end.offset}\n`;
+  string += `${" ".repeat(level * 2)}Rule@${start.offset}..${end.offset + 1}\n`;
   string += `${" ".repeat((level + 1) * 2)}selector: \`${node.raws?.selector?.raw ?? node.selector}\`\n`;
   return string;
 }
@@ -71,7 +68,7 @@ function generateAtRule(node, level, source) {
     source: { start, end },
   } = node;
   let string = "";
-  string += `${" ".repeat(level * 2)}AtRule@${start.offset}..${end.offset}\n`;
+  string += `${" ".repeat(level * 2)}AtRule@${start.offset}..${end.offset + 1}\n`;
   string += `${" ".repeat((level + 1) * 2)}name: \`${node.name}\`\n`;
   string += `${" ".repeat((level + 1) * 2)}params: \`${node.raws?.params?.raw ?? node.params}\`\n`;
   return string;
@@ -82,8 +79,17 @@ function generateDeclaration(node, level, source) {
     source: { start, end },
   } = node;
   let string = "";
-  string += `${" ".repeat(level * 2)}Declaration@${start.offset}..${end.offset}\n`;
+  string += `${" ".repeat(level * 2)}Declaration@${start.offset}..${end.offset + 1}\n`;
   string += `${" ".repeat((level + 1) * 2)}prop: \`${node.prop}\`\n`;
   string += `${" ".repeat((level + 1) * 2)}value: \`${node.raws?.value?.raw ?? node.value}\`\n`;
   return string;
 }
+const dir = "./custom-cases";
+let fileList = fs.readdirSync(dir);
+fileList.forEach(fileName => {
+  const concatPath = `${dir}/${fileName}`;
+  let file = fs.readFileSync(concatPath).toString();
+  const root = postcss.parse(file);
+  const astFile = generate(root, 0, file)
+  fs.writeFileSync(dir + '/' +   path.parse(fileName).name + ".ast", astFile)
+});
